ff91dfdf22d080f44f44b08fdcd4f0ac2665c14a
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2026-08-13 17:13:38 +0800

    Add user greeting and exit messages

--- src/main/java/Kachow.java
+++ src/main/java/Kachow.java
@@ -2,12 +2,21 @@
  * Starts the Kachow chatbot application.
  */
 public class Kachow {
+    private static final String DIVIDER = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " _  __          _                    \n"
                 + "| |/ /__ _  ___| |__   _____      __\n"
                 + "| ' // _` |/ __| '_ \\ / _ \\ \\ /\\ / /\n"
                 + "| . \\ (_| | (__| | | | (_) \\ V  V / \n"
                 + "|_|\\_\\__,_|\\___|_| |_|\\___/ \\_/\\_/  \n";
-        System.out.println(banner);
+
+        System.out.println(DIVIDER);
+        System.out.print(banner);
+        System.out.println("Ka-chow! I'm Kachow, the fastest chatbot on the track.");
+        System.out.println("What can I do for you before the next lap?");
+        System.out.println(DIVIDER);
+        System.out.println("Race complete! Catch you on the next lap. Ka-chow!");
+        System.out.println(DIVIDER);
     }
 }
