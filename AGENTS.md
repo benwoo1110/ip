b5188c323686ee5194f8e2cec5298474b59f8316
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2026-08-13 17:10:25 +0800

    Rename the chatbot to Kachow

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: >5000 lines of code
+* IDE and level of expertise: I use them regularly in my coding (more than 1000 lines of code)
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Kachow.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Kachow chatbot application.
+ */
+public class Kachow {
+    public static void main(String[] args) {
+        String banner = " _  __          _                    \n"
+                + "| |/ /__ _  ___| |__   _____      __\n"
+                + "| ' // _` |/ __| '_ \\ / _ \\ \\ /\\ / /\n"
+                + "| . \\ (_| | (__| | | | (_) \\ V  V / \n"
+                + "|_|\\_\\__,_|\\___|_| |_|\\___/ \\_/\\_/  \n";
+        System.out.println(banner);
+    }
+}
